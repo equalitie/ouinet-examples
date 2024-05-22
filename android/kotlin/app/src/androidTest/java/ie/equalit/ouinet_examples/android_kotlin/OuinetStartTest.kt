e57a4f6bd102724b3f1219d85d82a22d7a9567b6
Miguel <miguel@equalitie.org>
2024-05-22 00:13:30 -0600

    Click start button before checking the state of the service

--- android/kotlin/app/src/androidTest/java/ie/equalit/ouinet_examples/android_kotlin/OuinetStartTest.kt
+++ android/kotlin/app/src/androidTest/java/ie/equalit/ouinet_examples/android_kotlin/OuinetStartTest.kt
@@ -4,7 +4,7 @@ import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withText
-import androidx.test.core.app.ActivityScenario
+import androidx.test.espresso.action.ViewActions.click
 import androidx.test.ext.junit.rules.activityScenarioRule
 import androidx.test.filters.LargeTest
 import androidx.test.runner.AndroidJUnit4
@@ -18,14 +18,10 @@ class OuinetStartTest {
     @get:Rule
     var activityScenarioRule = activityScenarioRule<MainActivity>()
 
-    private fun checkOuinetStarted() {
-        /* Wait 10s for ouinet to fully start */
-        Thread.sleep(10000)
-        onView(withId(R.id.status)).check(matches(withText("State: Started")))
-    }
-
     @Test
     fun testOuinetStarted() {
-        checkOuinetStarted()
+        onView(withId(R.id.start)).perform(click())
+        Thread.sleep(10000)
+        onView(withId(R.id.status)).check(matches(withText("State: Started")))
     }
 }

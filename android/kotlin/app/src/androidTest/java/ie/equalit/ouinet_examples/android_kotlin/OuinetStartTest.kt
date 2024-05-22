4b690c82fa85aa1db166764e7ca77cf3b37dd301
paidforby <20746091+paidforby@users.noreply.github.com>
2024-05-22 11:07:02 -0400

    Add start_testsuite.sh and remove unneeded decorators from OuinetStartTest class

--- android/kotlin/app/src/androidTest/java/ie/equalit/ouinet_examples/android_kotlin/OuinetStartTest.kt
+++ android/kotlin/app/src/androidTest/java/ie/equalit/ouinet_examples/android_kotlin/OuinetStartTest.kt
@@ -6,14 +6,9 @@ import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.ext.junit.rules.activityScenarioRule
-import androidx.test.filters.LargeTest
-import androidx.test.runner.AndroidJUnit4
 import org.junit.Rule
 import org.junit.Test
-import org.junit.runner.RunWith
 
-@RunWith(AndroidJUnit4::class)
-@LargeTest
 class OuinetStartTest {
     @get:Rule
     var activityScenarioRule = activityScenarioRule<MainActivity>()
